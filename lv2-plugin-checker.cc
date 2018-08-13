32a849a43d1b5d7f0ea9a7e66531c6b1656e01c9
JP Cimalando <jpcima@users.noreply.github.com>
2018-08-13 16:21:18 +0200

    initialize control ports to default values

--- lv2-plugin-checker.cc
+++ lv2-plugin-checker.cc
@@ -7,6 +7,7 @@
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <cmath>
 
 static double sample_rate = 44100;
 static size_t buffer_size = 16;
@@ -74,8 +75,37 @@ bool test_plugin(LilvWorld &world, const char *uri)
     uint32_t num_ports = lilv_plugin_get_num_ports(plugin);
     std::unique_ptr<float[]> buffers(new float[num_ports * buffer_size]());
 
-    for (uint32_t i = 0; i < num_ports; ++i)
-        lilv_instance_connect_port(instance.get(), i, &buffers[i * buffer_size]);
+    std::unique_ptr<float[]> port_mins(new float[num_ports]());
+    std::unique_ptr<float[]> port_maxs(new float[num_ports]());
+    std::unique_ptr<float[]> port_defs(new float[num_ports]());
+
+    lilv_plugin_get_port_ranges_float(
+        plugin, port_mins.get(), port_maxs.get(), port_defs.get());
+
+    for (uint32_t i = 0; i < num_ports; ++i) {
+        const LilvPort *port = lilv_plugin_get_port_by_index(plugin, i);
+        LilvNode_u port_name_node(lilv_port_get_name(plugin, port));
+        const char *port_name = lilv_node_as_string(port_name_node.get());
+
+        float port_value = port_defs[i];
+        if (std::isnan(port_value))
+            port_value = port_mins[i];
+        if (std::isnan(port_value))
+            port_value = port_maxs[i];
+        if (std::isnan(port_value))
+            port_value = 0;
+
+        fprintf(stderr, "Port %u '%s': value %f min %f max %f default %f\n",
+                i, port_name, port_value, port_mins[i], port_maxs[i], port_defs[i]);
+
+        float *port_buffer = &buffers[i * buffer_size];
+        LilvNode_u cls_control_port(lilv_new_uri(&world, LILV_URI_CONTROL_PORT));
+        if (lilv_port_is_a(plugin, port, cls_control_port.get())) {
+            for (unsigned j = 0; j < buffer_size; ++j)
+                port_buffer[j] = port_value;
+        }
+        lilv_instance_connect_port(instance.get(), i, port_buffer);
+    }
 
     //
     lilv_instance_activate(instance.get());
